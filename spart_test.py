f8aaccb8a4fea896589f1774262b0a6985335084
Rupesh Basnet <rupeshbasnet93@gmail.com>
2018-04-24 14:35:19 -0400

    Update spart_test.py

--- spart_test.py
+++ spart_test.py
@@ -3,7 +3,6 @@ import pyspark
 if __name__ == '__main__':
 	sc = pyspark.SparkContext()
 	RES_Data = 'hdfs:///data/share/bdm/nyc_restaurants.csv'
-	print sys.argv
 	df = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load(RES_Data)
 	cusines = df.select('CUISINE DESCRIPTION')
 	cusines = cusines.groupBy('CUISINE DESCRIPTION').count().orderBy('count', ascending=False)
